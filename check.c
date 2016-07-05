f676dcf71be26ec5f81252da2e92a83ea8ed3561
Alexander Maringele <amaringele@am-x240.uibk.ac.at>
2016-07-05 15:45:36 +0200

    Update check

--- check.c
+++ check.c
@@ -5,24 +5,57 @@
 void print_memory_info(const char* name, const prlc_memory memory);
 void print_store_infos(PrlcStoreRef store);
 
+void check_memory();
 void check_string_store();
-void check_parse_puz001();
+void check_parse_file(const char *);
+
+const char* puz001="/home/amaringele/Downloads/TPTP/Problems/PUZ/PUZ001-1.p";
+const char* hwv = "/home/amaringele/Downloads/TPTP/Problems/HWV/HWV003-1.p";
+const char* hwv134 = "/home/amaringele/Downloads/TPTP/Problems/HWV/HWV134-1.p";
 
 int main()
 {
-	printf("Hello World!\n");
 
-	check_string_store ();
+	// check_string_store ();
 
-	check_parse_puz001 ();
+	check_parse_file (puz001);
+
+	// check_memory ();
+
+/*	FILE *file = fopen(hwv134,"r");
+
+	
+
+	int size = file_size(file);
+
+	printf("%d, %s\n", size, hwv134);
+	
+	fclose(file);*/
 	
 	return (0);
 }
 
-void check_parse_puz001() {
+void check_memory() {
+	// 276455091, /home/amaringele/Downloads/TPTP/Problems/HWV/HWV134-1.p
+	long size = 276455091/4;
+	while (size < 2764550910) {
+
+		printf("%ld\n",size);
+
+		PrlcStoreRef store = prlcCreateStore(size);
+
+		print_store_infos (store);
+
+		prlcDestroyStore(&store);
+		size *= 2;
+	}
+}
+
+void check_parse_file(const char *path) {
+	printf("%s",path);
 	
 
-	prlc_parse_path ("/home/amaringele/Downloads/TPTP/Problems/PUZ/PUZ001-1.p");
+	prlc_parse_path (path);
 	PrlcStoreRef store = prlcParsingStore;
 	prlc_tree_node* root = prlcParsingRoot;
 	
