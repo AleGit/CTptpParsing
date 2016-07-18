44d63226819ca4ecf0bfce88409c645b0cd2d025
Alexander Maringele <aXm@ydWLT-E-18-34.uibk.ac.at>
2016-07-18 10:17:16 +0200

    Add critical section

--- CSources/PrlcParser.c
+++ CSources/PrlcParser.c
@@ -3,6 +3,7 @@
 
 #import "PrlcParser.h"
 #import <sys/sysctl.h>
+#import <pthread.h>
 
 prlc_store* prlcParsingStore = NULL;
 prlc_tree_node* prlcParsingRoot = NULL;
@@ -89,51 +90,58 @@ prlc_tree_node* prlc_minimize_store(prlc_store *store) {
   return store->t_nodes.memory;
 }
 
+pthread_mutex_t parsemutex = PTHREAD_MUTEX_INITIALIZER;
 
 int prlcParseFile(const char * const path, prlc_store** store, prlc_tree_node** root) {
-
-    if (path == NULL) {
-      fprintf(stderr,"prlc_parse_path(NULL)\n");
-      return -1;
-    }
-
-    FILE *file = fopen(path,"r");
-
-    int size = prlc_file_size(file);
-
-    if (file == NULL) {
-      // file not found
-      fprintf(stderr,"prlc_parse_path(%s) file could not be oppened.\n", path);
-      return -1;
-    }
-
-    if (size == 0) {
-      // file is empty
-      fprintf(stderr,"prlc_parse_path(%s) file is empty.\n", path);
-      return -1;
-    }
-
-    prlc_in = file;
-    prlc_restart(file);
-    prlc_lineno = 1;
-
-    prlcParsingStore = prlcCreateStore(size);
-    prlcParsingRoot = prlcStoreNodeFile (prlcParsingStore,path);
-
-    int code = prlc_parse ();
-
-    fclose(file);
-
-    *store = prlcParsingStore;
-    *root = prlcParsingRoot;
-
-    // it seems as memory optimization is not needed
-    // but intruduces small runtime overhead and
-    // additional failure possibilites.
-    // *root = prlc_minimize_store(*store);
-
-    prlcParsingStore = NULL;
-    prlcParsingRoot = NULL;
-
-    return code;
+  if (path == NULL) {
+    fprintf(stderr,"prlc_parse_path(NULL)\n");
+    return -1;
   }
+
+  FILE *file = fopen(path,"r");
+
+  int size = prlc_file_size(file);
+
+  if (file == NULL) {
+    // file not found
+    fprintf(stderr,"prlc_parse_path(%s) file could not be oppened.\n", path);
+    return -1;
+  }
+
+  if (size == 0) {
+    // file is empty
+    fprintf(stderr,"prlc_parse_path(%s) file is empty.\n", path);
+    return -1;
+  }
+
+  /* enter critical section */
+  pthread_mutex_lock(&parsemutex);
+
+  prlc_in = file;
+  prlc_restart(file);
+  prlc_lineno = 1;
+
+  prlcParsingStore = prlcCreateStore(size);
+  prlcParsingRoot = prlcStoreNodeFile (prlcParsingStore,path);
+
+  int code = prlc_parse ();
+
+  fclose(file);
+
+  *store = prlcParsingStore;
+  *root = prlcParsingRoot;
+
+  // it seems as memory optimization is not needed
+  // but intruduces small runtime overhead and
+  // additional failure possibilites.
+  // *root = prlc_minimize_store(*store);
+
+  prlcParsingStore = NULL;
+  prlcParsingRoot = NULL;
+
+
+  /* leave critical section */
+  pthread_mutex_unlock(&parsemutex);
+
+  return code;
+}
